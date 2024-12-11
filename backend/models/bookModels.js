4d653c89c7ab1e88b1fb442d227e4772ed402d0f
Shariif-Rajpoot <mr8781256@gmail.com>
2024-12-11 21:24:09 +0500

    Basic Setup

---
+++ backend/models/bookModels.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+// creating a schema
+const bookSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    publishYear: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// creating model
+export const Book = mongoose.model("Cat", bookSchema);
