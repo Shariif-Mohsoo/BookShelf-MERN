d81afe4026b28fa27aa775e3f498e43b908926f9
Shariif-Rajpoot <mr8781256@gmail.com>
2024-12-13 21:32:15 +0500

    Main Entry Point

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter} from'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import './index.css';
+import App from "./App";
+
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el);
+root.render(
+<BrowserRouter>
+<App />
+</BrowserRouter>
+);
