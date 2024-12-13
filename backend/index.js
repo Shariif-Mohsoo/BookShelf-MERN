29b64b4a1690488b47d67227b2b41fdaa42a3ac0
Shariif-Rajpoot <mr8781256@gmail.com>
2024-12-13 19:52:34 +0500

    Server setup

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 // console.log("Hello Mohsoo");
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 
 import { port, mongoDBURL } from "./config.js";
 import booksRoute from "./routes/booksRoute.js";
@@ -9,6 +10,14 @@ const app = express();
 
 // Middleware for parsing the request body
 app.use(express.json());
+// Option 1: Allow all origins with default of cors(*)
+app.use(cors());
+// Option2 : Allow custom origins
+// app.use({
+//   origin: "http://localhost:3000",
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   allowedHeaders: ["Content-Type"],
+// });
 
 app.get("/", (req, res) => {
   res.status(200).send("Welcome to MERN tutorial");
